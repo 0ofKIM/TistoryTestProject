ecdbde3d966fb432264eaeed3525ec511ae39f79
uykim3 <uykim3@lotte.net>
2019-12-01 20:34:48 +0900

    first commit

---
+++ TistoryTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TistoryTestProject
+//
+//  Created by Lotte on 2019/12/01.
+//  Copyright © 2019 0ofKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
